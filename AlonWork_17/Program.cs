6aff9dbc73895f584f9845bdd62fb236a667727c
Дмитрий Изох <izokh_dv@mail.ru>
2022-11-20 19:00:04 +0300

    Добавьте файлы проекта.

---
+++ AlonWork_17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlonWork_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Сheck<int> check1 = new Сheck<int>();
+            check1.Input();
+            Console.WriteLine(check1.GetInfo());
+
+            Сheck<string> check2 = new Сheck<string>();
+            check2.Input();
+            Console.WriteLine(check2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AlonWork_17/Сheck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlonWork_17
+{
+    class Сheck <T>
+    {
+        public T Num { get; set; }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+
+        public string GetInfo()
+        {
+            return $"{Num} {Balance} {Name}";
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер");
+            string stringNum = Console.ReadLine();
+            Num = (T)Convert.ChangeType(stringNum, typeof(T));
+
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите Имя");
+            Name = Console.ReadLine();
+        }
+    }
+}
